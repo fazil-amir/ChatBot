f75afad6a40b9d3073e6c7773f1ade953457d72a
FazilAmir <webmasterfazil@gmail.com>
2016-08-14 19:28:37 +0200

    A

---
+++ Dictionary.js
@@ -0,0 +1,58 @@
+var dictionary =
+
+[
+	{
+		"inputString": [
+			"init"
+		],
+		"responseString": [
+			"Hi! what a lovely day",
+			"Hey user welcome",
+			"Hey folks! we are bots",
+			"Hello guys!"
+		]
+	},
+	{		
+		"inputString": [
+			"hi how are you",
+			"hey how you doing",
+			"hello how are you"
+		],
+		"responseString": [
+			"I'm good thanks! how you?",
+			"I'm good how about you?",
+			"",
+			"Great! you say"
+		]
+	},
+	{
+		"inputString": [
+			"hello",
+			"hi",
+			"hey",
+			"wassup",
+			"whats up",
+			"ssup"
+		],
+		"responseString" : [
+			"Hello user",
+			"Hi boss",
+			"Heya!",
+			"Heeeeeeeelo! :D"
+		]
+	},
+	{
+		"inputString": [
+			"what is your name",
+			"whats your name",
+			"your name",
+			"do you have a name"
+		],
+		"responseString" : [
+			"My name? I'm a bot",
+			"I'm bot",
+			"Me either",
+			""
+		]
+	}
+];
